6e6d9108f6dbac157b2416298988bb7cccf6d069
Dusan Mitrovic <dusanmitrovic@elfak.rs>
2023-02-03 12:20:35 +0100

    Pre-Server State.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
+import managementRoutes from "./routes/management.js";
+import generalRoutes from "./routes/general.js";
+import clientRoutes from "./routes/client.js";
+import salesRoutes from "./routes/sales.js";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
-import generalRoutes from "./routes/general.js";
-import clientRoutes from "./routes/client.js";
-import managementRoutes from "./routes/management.js";
-import salesRoutes from "./routes/sales.js";
 
 /* CONFIGURATION */
 dotenv.config();
