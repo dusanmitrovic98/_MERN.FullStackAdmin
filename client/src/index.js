de9ba35100533089fb32c9f40cd294e9f2de2c9e
Dusan Mitrovic <dusanmitrovic@elfak.rs>
2023-02-14 23:24:23 +0100

    Added StatBox component and fixes.

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import globalReducer from "state";
 import React from "react";
-import App from "./App";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "state/api";
 
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
